59399945e2b05b589ef3054cd4c5e205dc1c42e5
hydrajump <wave@hydrajump.com>
2014-08-15 20:25:04 -0700

    Add IBOutlets and IBActions connecting the storyboard and CoinPairsModel

--- CoinPairs/CoinPairs/ViewController.swift
+++ CoinPairs/CoinPairs/ViewController.swift
@@ -9,7 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+
+    @IBOutlet weak var coinPairDiffLabel: UILabel!
+    @IBOutlet weak var coinPairDiffSlider: UISlider!
+    @IBOutlet weak var coinsTextField: UITextField!
+
+    @IBAction func coinPairDiffChanged(sender: AnyObject) {
+        coinPairDiffLabel.text = "Coin Pair Difference (\(Int(coinPairDiffSlider.value))):"
+    }
+
+    @IBAction func findCoinPairsTapped(sender: AnyObject) {
+
+        let coinPairDiff = Int(coinPairDiffSlider.value)
+
+        let coins = coinsTextField.text.componentsSeparatedByString(",")
+        println(coins)
+
+        var userCoins: [Int] = []
+        for coin in coins {
+            println(coin.toInt()!)
+            userCoins += [coin.toInt()!]
+        }
+        println(userCoins)
+
+        let coinPairs = CoinPairsModel(coins: userCoins, coinPairDiff: coinPairDiff)
+        coinPairs.findCoinPairs()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +45,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
